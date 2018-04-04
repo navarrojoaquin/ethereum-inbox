e011ee22b2053489025480c045c4a0b5624b56ca
Joaquín Navarro <joaquin.navarro.s@gmail.com>
2018-04-04 18:27:02 +0200

    Add missing semicolon

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -13,7 +13,7 @@ beforeEach(async () => {
     // Use an account to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hello!'] })
-        .send({ from: accounts[0], gas: '1000000' })
+        .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
